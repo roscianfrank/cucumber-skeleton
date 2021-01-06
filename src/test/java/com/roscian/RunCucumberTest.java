940bf9b9b898694a67e10d075fcb469b5a25d2a6
Roscian Frank <rfrank@thecollective.com>
2021-01-06 10:14:44 +0000

    Folders rearranged

--- src/test/java/com/roscian/RunCucumberTest.java
+++ src/test/java/com/roscian/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","html:target/cucumber-report"},
+@CucumberOptions(plugin = {"pretty"},
         features = "src/test/resources/Features/",
         glue = "com.roscian")
 public class RunCucumberTest {
